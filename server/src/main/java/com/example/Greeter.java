4029b4465d142cc87cb2a28e9c91757897d462c2
Simon Frezard <simon.frezard@gmail.com>
2024-04-19 12:57:07 +0200

    fix(update)

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   // TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello my dear twice abc, %s!", someone);
+    return String.format("Hello my dear twice ab test 3, %s!", someone);
   }
 }
